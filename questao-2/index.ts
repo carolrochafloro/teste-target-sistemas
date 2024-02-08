708f29bc614967bfe9212cf79a7f75357c7e6c54
carolrochafloro <carolinarochaf@hotmail.com>
2024-02-08 18:14:49 -0300

    Ajuste questão 2 - troca let por const

--- questao-2/index.ts
+++ questao-2/index.ts
@@ -1,14 +1,13 @@
 /* Recebe um número como parâmetro, calcula a sequência até o próximo número maior do que a entrada e verifica se o número desejado pertence. */
 
 function isNumberInFibonacciSequence(num: number): string {
-  let sequence = [0, 1];
+  const sequence = [0, 1];
   while (sequence[sequence.length - 1] < num) {
     sequence.push(sequence[sequence.length - 2] + sequence[sequence.length - 1]);
   }
   if (!sequence.includes(num)) {
     return `${num} não pertence à sequência de Fibonacci.`;
   }
-
     return `${num} pertence à sequência de Fibonacci.`;
 }
 
